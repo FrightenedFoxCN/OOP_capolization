1b3ff84519f569e4a490983f4aa867c3ab8dd05e
yhwu-is <2437072836@qq.com>
2022-05-04 20:10:55 +0800

    add json lib

--- src/utils/json_parser.cpp
+++ src/utils/json_parser.cpp
@@ -4,4 +4,3 @@ template <class T>
 Json::Reader<T>::Reader(const std::string &filename){
     this->is.open(filename, std::ios::in);
 }
-
--- src/utils/json_value.cpp
+++ src/utils/json_value.cpp
@@ -26,5 +26,49 @@ Value::Value(std::string string_) {
     this->value.string_ = new std::string(string_);
 }
 
-    // Value(bool bool_);
-    // Value(std::pair<std::string, Value> *pair_);
+Value::Value(bool bool_)
+{
+    this->type = boolValue;
+    this->value.bool_ = bool_;
+}
+
+Value::Value(std::pair<std::string, Value> *pair_)
+{
+    this->type = pairwiseValue;
+    this->value.pair_ = pair_;
+}
+
+bool Value::isNull(void)
+{
+    return this->type == nullValue;
+}
+
+bool Value::isUInt(void)
+{
+    return this->type == uintValue;
+}
+
+bool Value::isReal(void)
+{
+    return this->type == realValue;
+}
+
+bool Value::isString(void)
+{
+    return this->type == stringValue;
+}
+
+bool Value::isArray(void)
+{
+    return this->type == arrayValue;
+}
+
+bool Value::isBool(void)
+{
+    return this->type == boolValue;
+}
+
+bool Value::isPairwise(void)
+{
+    return this->type == pairwiseValue;
+}
