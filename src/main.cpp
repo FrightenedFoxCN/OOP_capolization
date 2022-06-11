1852033bad76a5b473e6b80376ecd322279da3b3
yhwu-is <2437072836@qq.com>
2022-06-11 17:34:34 +0800

    fix file create error

--- src/main.cpp
+++ src/main.cpp
@@ -45,14 +45,8 @@ int main() {
       Sleep(1000);
       std::string nextDial =  dial.getNextDialog();
       if (nextDial.length() == 0) {
-         if (f.good()) {
-            f.open(userFileName, std::ios::out);
-            f << std::setfill('0') << std::setw(5) << dial.getDialogId();
-         }
-         else {
-            f.open(userFileName, std::ios::ate);
-            f << std::setfill('0') << std::setw(5) << dial.getDialogId();
-         }
+         f.open(userFileName, std::ios::out);
+         f << std::setfill('0') << std::setw(5) << dial.getDialogId();
          f.close();
          return 0;
       }
